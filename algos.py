b49577a8a736ac8ea5e86894554cefae57d82050
Anirudh Krishna <anirudhk@ucla.edu>
2024-05-26 18:02:31 -0700

    Qtest1

--- algos.py
+++ algos.py
@@ -2,6 +2,35 @@ import numpy as np
 import random
 
 
+def naive_testing(s):
+    # Fix test count
+    num = 0
+    stages = 1
+    positive_tests = 0
+    negative_tests = 0
+
+    proportion_of_positive_tests = sum(s)/len(s)
+    num+=1
+    
+    if(proportion_of_positive_tests >= 0.5):
+      new_s = np.zeros(len(s))
+      for i in range(len(s)):
+          if s[i] == 1:
+            new_s[i] = 1
+            positive_tests += 1
+          num+=1
+          if positive_tests==sum(s): break
+    else:
+      new_s = np.ones(len(s))
+      for i in range(len(s)):
+          if s[i] == 0:
+            new_s[i] = 0
+            negative_tests += 1
+          num+=1
+          if negative_tests==(len(s)-sum(s)): break
+
+    return num, stages, new_s
+
 # binary spliting
 def binary_splitting_round(s):
     # s: np.array the infectious status & test status
@@ -98,19 +127,56 @@ def diag_splitting(s):
                 flag = 1
     return num_tests,stages
 
+def Qtesting1_iter(s):
+    # s(np.array): binary string of infection status
+    # Ex: [1,0,0,1,0,0,1,0]
+    # k = 3 (iteration number)
+    k = int(np.log2(len(s)))
+    # l = 4
+    l = int(2**(k-1))
+    lp = 0
+    # p = [0,0,0,0] (num_of_groups)
+    p = np.zeros(k+1)
+    group = dict()
+    # num = [1,1,1,1]
+    num = np.ones(k+1,dtype=np.int32)
+    for i in range(k):
+        p[i] = sum(s[lp:lp+l])>0
+        group[i] = s[lp:lp+l]
+        num[i] = l
+        lp+=l
+        l = l//2
+
+    p[-1] = s[-1]
+    group[k] = np.array([s[-1]])
+    # p(array): pattern
+    # group(dict): indicate the group information
+    # num(array): the group size
+    return p.astype(np.int32), group,num
+
 def Qtesting1(s):
-    '''
-    s(np.array): binary string of infection status
-    '''
+    if sum(s)/len(s) > 0.25:
+        num, stages, _ = naive_testing(s)
+        return num, stages
+
+    # s(np.array): binary string of infection status
     num_tests = 0
     stages = 0
-    ###################################################
-    '''your code here'''
+    pattern, group, nums = Qtesting1_iter(s)
+    stages +=1
+    # pattern is the test_result for each group
+    num_tests += len(pattern)
 
-    ###################################################
-
-
-
+    # All groups which tested positive
+    indices = np.where(pattern == 1)[0]
+    flag = 0
+    for i in indices:
+        if nums[i]>1:
+            num_test,stage = Qtesting1(group[i])
+            num_tests += num_test
+            if not flag:
+                stages+=stage
+                flag = 1
     return num_tests,stages
 
 
